dead08316f67c163da1a04cf0aab968e7f171918
Krishna-Dhawan <85281882+Krishna-Dhawan@users.noreply.github.com>
2025-04-22 23:18:27 +0530

    drop proc if exists

--- procedures.sql
+++ procedures.sql
@@ -8,30 +8,35 @@ BEGIN
     VALUES (name, address, phone);
 END //
 
+DROP PROCEDURE IF EXISTS AddPharmaCompany //
 CREATE PROCEDURE AddPharmaCompany(IN name VARCHAR(30), IN phone VARCHAR(15))
 BEGIN
     INSERT INTO PharmaceuticalCompany (CoName, PhoneNumber)
     VALUES (name, phone);
 END //
 
+DROP PROCEDURE IF EXISTS AddDoctor //
 CREATE PROCEDURE AddDoctor(IN aadhar CHAR(12), IN name VARCHAR(50), IN specialization VARCHAR(100), IN experience INT)
 BEGIN
     INSERT INTO Doctor (dAadharID, dName, Specialization, Experience)
     VALUES (aadhar, name, specialization, experience);
 END //
 
+DROP PROCEDURE IF EXISTS AddPatient //
 CREATE PROCEDURE AddPatient(IN aadhar CHAR(12), IN name VARCHAR(50), IN address VARCHAR(200), IN age INT, IN physician CHAR(12))
 BEGIN
     INSERT INTO Patient (pAadharID, pName, pAddress, Age, PrimaryPhysicianID)
     VALUES (aadhar, name, address, age, physician);
 END //
 
+DROP PROCEDURE IF EXISTS AddDrug //    
 CREATE PROCEDURE AddDrug(IN name VARCHAR(30), IN formula TEXT, IN company VARCHAR(30))
 BEGIN
     INSERT INTO Drug (TradeName, Formula, CoName)
     VALUES (name, formula, company);
 END //
 
+DROP PROCEDURE IF EXISTS AddContract //
 CREATE PROCEDURE AddContract(IN company VARCHAR(30), IN name VARCHAR(100), IN address VARCHAR(100), IN startDate DATE, IN endDate DATE, IN content TEXT, IN supervisor CHAR(12))
 BEGIN
     INSERT INTO Contract (CompanyName, PharmacyName, PharmacyAddress, StartDate, EndDate, Content, SupervisorID)
@@ -40,6 +45,7 @@ END //
 
 -- UPDATE Procedures
 
+DROP PROCEDURE IF EXISTS UpdatePharmacyPhone //
 CREATE PROCEDURE UpdatePharmacyPhone(IN name VARCHAR(100), IN address VARCHAR(100), IN newPhone VARCHAR(15))
 BEGIN
     UPDATE Pharmacy
@@ -47,6 +53,7 @@ BEGIN
     WHERE PharmacyName = name AND PharmacyAddress = address;
 END //
 
+DROP PROCEDURE IF EXISTS UpdateDoctorExperience //
 CREATE PROCEDURE UpdateDoctorExperience(IN aadhar CHAR(12), IN newExp INT)
 BEGIN
     UPDATE Doctor
@@ -56,11 +63,13 @@ END //
 
 -- DELETE Procedures
 
+DROP PROCEDURE IF EXISTS DeleteDoctor //
 CREATE PROCEDURE DeleteDoctor(IN aadhar CHAR(12))
 BEGIN
     DELETE FROM Doctor WHERE dAadharID = aadhar;
 END //
 
+DROP PROCEDURE IF EXISTS DeletePharmacy //
 CREATE PROCEDURE DeletePharmacy(IN name VARCHAR(100), IN address VARCHAR(100))
 BEGIN
     DELETE FROM Pharmacy WHERE PharmacyName = name AND PharmacyAddress = address;
@@ -68,36 +77,42 @@ END //
 
 -- Reporting Procedures
 
+DROP PROCEDURE IF EXISTS PrescriptionsByPatientPeriod //
 CREATE PROCEDURE PrescriptionsByPatientPeriod(IN patientID CHAR(12), IN fromDate DATE, IN toDate DATE)
 BEGIN
     SELECT * FROM Prescribes
     WHERE pAadharID = patientID AND prescriptionDate BETWEEN fromDate AND toDate;
 END //
 
+DROP PROCEDURE IF EXISTS PrescriptionDetailsByDate //
 CREATE PROCEDURE PrescriptionDetailsByDate(IN patientID CHAR(12), IN prescDate DATE)
 BEGIN
     SELECT * FROM Prescribes
     WHERE pAadharID = patientID AND prescriptionDate = prescDate;
 END //
 
+DROP PROCEDURE IF EXISTS DrugsByCompany //
 CREATE PROCEDURE DrugsByCompany(IN companyName VARCHAR(30))
 BEGIN
     SELECT TradeName, Formula FROM Drug
     WHERE CoName = companyName;
 END //
 
+DROP PROCEDURE IF EXISTS StockByPharmacy //
 CREATE PROCEDURE StockByPharmacy(IN name VARCHAR(50), IN location VARCHAR(100))
 BEGIN
     SELECT DrugName, Price FROM Sells
     WHERE PharmacyName = name AND Phloc = location;
 END //
 
+DROP PROCEDURE IF EXISTS ContactDetailsPharmacyPharma //
 CREATE PROCEDURE ContactDetailsPharmacyPharma(IN name VARCHAR(100), IN address VARCHAR(100), IN company VARCHAR(30))
 BEGIN
     SELECT * FROM Contract
     WHERE PharmacyName = name AND PharmacyAddress = address AND CompanyName = company;
 END //
 
+DROP PROCEDURE IF EXISTS PatientsByDoctor //
 CREATE PROCEDURE PatientsByDoctor(IN doctorID CHAR(12))
 BEGIN
     SELECT pName, pAadharID FROM Patient
