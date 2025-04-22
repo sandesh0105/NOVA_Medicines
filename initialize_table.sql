abaf44a1ecef12efd186fa2760300f1025c9bdf2
Krishna-Dhawan <dhawan.krish@gmail.com>
2025-04-22 22:18:16 +0530

    initializeTableUpdate

--- initialize_table.sql
+++ initialize_table.sql
@@ -1,23 +1,28 @@
+-- DBMS Project
+-- source <file path>/<filename>.sql;
+DROP DATABASE IF EXISTS NOVA;
 create database NOVA;
 
-create table Patient (
-    pAadharID CHAR(12) PRIMARY KEY,
-    pName VARCHAR(50),
-    pAddress VARCHAR(200),
-    Age INT,
-    PrimaryPhysicianID CHAR(12),
-    FOREIGN KEY (PrimaryPhysicianID) REFERENCES Doctor(AadharID)
-);
-
+FLUSH PRIVILEGES;
+use NOVA;
 create table Doctor (
     dAadharID CHAR(12) PRIMARY KEY,
     dName VARCHAR(50),
     Specialization VARCHAR(100),
     Experience INT
 );
+create table Patient (
+    pAadharID CHAR(12) PRIMARY KEY,
+    pName VARCHAR(50),
+    pAddress VARCHAR(200),
+    Age INT,
+    PrimaryPhysicianID CHAR(12),
+    FOREIGN KEY (PrimaryPhysicianID) REFERENCES Doctor(dAadharID)
+);
+
 
 create table PharmaceuticalCompany (
-    CoName VARCHAR(30),
+    CoName VARCHAR(30) PRIMARY KEY,
     PhoneNumber VARCHAR(15)
 );
 
@@ -28,7 +33,6 @@ create table Drug (
     PRIMARY KEY (TradeName, CoName),
     FOREIGN KEY (CoName) REFERENCES PharmaceuticalCompany(CoName) ON DELETE CASCADE
 );
-
 CREATE TABLE Pharmacy (
     PharmacyName VARCHAR(100) PRIMARY KEY,
     PharmacyAddress TEXT,
@@ -38,13 +42,12 @@ CREATE TABLE Pharmacy (
 create table Sells (
     PharmacyName VARCHAR(50),
     DrugName VARCHAR(30),
-    Phloc VARCHAR(30),
     Price DECIMAL(10, 2),
-    PRIMARY KEY (PharmacyName, DrugName,Phloc),
+    PRIMARY KEY (PharmacyName, DrugName),
     FOREIGN KEY (PharmacyName) REFERENCES Pharmacy(PharmacyName),
     FOREIGN KEY (DrugName) REFERENCES Drug(TradeName)
-    FOREIGN KEY (Phloc) REFERENCES Pharmacy(PharmacyAddress)
 );
+
 
 create table Prescribes (
     prescriptionDate DATE,
@@ -62,13 +65,12 @@ CREATE TABLE Contract (
     -- ContractID SERIAL PRIMARY KEY,
     PharmacyName VARCHAR(100),
     CompanyName VARCHAR(100),
-    PharmacyAddress VARCHAR(100),
     StartDate DATE,
     EndDate DATE,
     Content TEXT,
     SupervisorID CHAR(12),
-    PRIMARY KEY (PharmacyName, CompanyName,PharmacyAddress),
+    PRIMARY KEY (PharmacyName, CompanyName),
     FOREIGN KEY (PharmacyName) REFERENCES Pharmacy(PharmacyName),
     FOREIGN KEY (CompanyName) REFERENCES PharmaceuticalCompany(CoName),
-    FOREIGN KEY (PharmacyAddress) REFERENCES Pharmacy(PharmacyAddress)
+    FOREIGN KEY (SupervisorID) REFERENCES Doctor(dAadharID)
 );
